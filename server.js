c9e4ce3a21684ee5f5f53a2b922fd9bd1339d216
James Canlas <jamescanlasjr@yahoo.com>
2021-03-18 18:10:36 -0700

    added partials, for comments and post info, fixed pural, date, and url formating as well

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const exphbs = require('express-handlebars');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
